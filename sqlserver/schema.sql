045d28d5a9b8358ae7e980667adc8c78bbd7a65b
kwalter <walterkaunda@live.co.uk>
2021-11-25 09:45:58 +0200

    Added logstash jdbc-output-plugin installation step

---
+++ sqlserver/schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE kibana_sample_ecommerce;
+
+CREATE TABLE Customers (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    CustomerFirstName VARCHAR(64),
+    CustomerLastName VARCHAR(64),
+    CustomerGender VARCHAR(12)
+)
+
